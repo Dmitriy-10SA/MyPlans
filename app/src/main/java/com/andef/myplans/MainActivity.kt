3ce294c6ba38127ea5711e27b50ef4ab84d71ab6
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-01-22 00:18:15 +0400

    Main screen

--- app/src/main/java/com/andef/myplans/MainActivity.kt
+++ app/src/main/java/com/andef/myplans/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.andef.myplans
 
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.applandeo.materialcalendarview.CalendarView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerViewPlansInPlans: RecyclerView
+    private lateinit var recyclerViewPlansInCalendar: RecyclerView
+
+    private lateinit var calendarView: CalendarView
+
+    private lateinit var cardViewPlans: CardView
+    private lateinit var cardViewCalendar: CardView
+
+    private lateinit var floatingActionButtonAddPlan: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +32,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initViews()
+    }
+
+    private fun initViews() {
+        recyclerViewPlansInPlans = findViewById(R.id.recyclerViewPlansInPlans)
+        recyclerViewPlansInCalendar = findViewById(R.id.recyclerViewPlansInCalendar)
+
+        calendarView = findViewById(R.id.calendarView)
+
+        cardViewPlans = findViewById<CardView?>(R.id.cardViewPlans).apply {
+            setOnClickListener {
+                recyclerViewPlansInCalendar.visibility = GONE
+                calendarView.visibility = GONE
+                recyclerViewPlansInPlans.visibility = VISIBLE
+            }
+        }
+        cardViewCalendar = findViewById<CardView?>(R.id.cardViewCalendar).apply {
+            setOnClickListener {
+                recyclerViewPlansInPlans.visibility = GONE
+                calendarView.visibility = VISIBLE
+                recyclerViewPlansInCalendar.visibility = VISIBLE
+            }
+        }
+
+        floatingActionButtonAddPlan = findViewById(R.id.floatingActionButtonAddPlan)
     }
 }
