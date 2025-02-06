f133473483ac2c686a256040a492fe688cad41ca
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-02-07 02:21:54 +0400

    Finished project

--- app/src/main/java/com/andef/myplans/presentation/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/andef/myplans/presentation/ui/viewmodel/MainViewModel.kt
@@ -3,6 +3,7 @@ package com.andef.myplans.presentation.ui.viewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.andef.myplans.domain.entities.Plan
 import com.andef.myplans.domain.usecases.GetPlans
@@ -15,12 +16,15 @@ import io.reactivex.schedulers.Schedulers
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     private val compositeDisposable = CompositeDisposable()
 
-    val plansInPlans = MutableLiveData<List<Plan>>()
-    val plansInCalendar = MutableLiveData<List<Plan>>()
+    private val _plansInPlans = MutableLiveData<List<Plan>>()
+    val plansInPlans: LiveData<List<Plan>> = _plansInPlans
+
+    private val _plansInCalendar = MutableLiveData<List<Plan>>()
+    val plansInCalendar: LiveData<List<Plan>> = _plansInCalendar
 
     fun loadPlansInPlans(lifecycleOwner: LifecycleOwner) {
         GetPlans.execute(getApplication()).observe(lifecycleOwner) {
-            plansInPlans.value = it
+            _plansInPlans.value = it
         }
     }
 
@@ -34,7 +38,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     fun loadPlansByDate(lifecycleOwner: LifecycleOwner, date: String) {
         GetPlansByDate.execute(getApplication(), date).observe(lifecycleOwner) {
-            plansInCalendar.value = it
+            _plansInCalendar.value = it
         }
     }
 
